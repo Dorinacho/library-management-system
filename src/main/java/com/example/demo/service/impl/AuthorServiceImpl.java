10ddfa206c4023a7e63e19995744d28a50e55f87
Dorinacho <dorinsuciu1999@yahoo.com>
2024-07-08 17:08:41 +0300

    Improved error handling

--- src/main/java/com/example/demo/service/impl/AuthorServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AuthorServiceImpl.java
@@ -1,13 +1,13 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.mapper.AuthorMapper;
-import com.example.demo.mapper.BookMapper;
 import com.example.demo.dto.author.SaveAuthorDto;
 import com.example.demo.dto.author.SimpleViewAuthorDto;
 import com.example.demo.dto.author.ViewAuthorDto;
 import com.example.demo.entity.Author;
+import com.example.demo.mapper.AuthorMapper;
 import com.example.demo.repository.AuthorRepository;
 import com.example.demo.service.AuthorService;
+import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -19,11 +19,11 @@ public class AuthorServiceImpl implements AuthorService {
 
   private final AuthorRepository authorRepository;
   private final AuthorMapper authorMapper;
-  private final BookMapper bookMapper;
 
   @Override
   public ViewAuthorDto getAuthorById(UUID authorId) {
-    Author author = authorRepository.findById(authorId).orElseThrow();
+    Author author = authorRepository.findById(authorId).orElseThrow(
+        () -> new EntityNotFoundException("Author with id: '" + authorId + "' couldn't be found"));
     return authorMapper.toViewAuthorDto(author);
   }
 
@@ -42,7 +42,8 @@ public class AuthorServiceImpl implements AuthorService {
 
   @Override
   public void updateAuthor(UUID authorId, SaveAuthorDto saveAuthorDto) {
-    Author oldAuthor = authorRepository.findById(authorId).orElseThrow();
+    Author oldAuthor = authorRepository.findById(authorId).orElseThrow(
+        () -> new EntityNotFoundException("Author with id: '" + authorId + "' couldn't be found"));
     Author updatedAuthor = authorMapper.toAuthor(oldAuthor, saveAuthorDto);
     authorRepository.save(updatedAuthor);
   }
